ac3ebb57a65a2b91d6eddc934e46112c9986ef77
qux-bbb <1147635419@qq.com>
2021-08-01 23:37:22 +0800

    calculate by block for large file

--- qhash.py
+++ qhash.py
@@ -6,6 +6,8 @@ import sys
 import binascii
 from hashlib import md5, sha1, sha256, sha512
 
+BLOCK_SIZE = 1024*1024
+
 
 def calc_hash(file_path):
     """calc many hash
@@ -15,20 +17,63 @@ def calc_hash(file_path):
     """
     result = {}
     the_file = open(file_path, 'rb')
-    file_content = the_file.read()
 
-    result['crc32'] = hex(binascii.crc32(file_content))[2:]
-    result['md5'] = md5(file_content).hexdigest()
-    result['sha1'] = sha1(file_content).hexdigest()
-    result['sha256'] = sha256(file_content).hexdigest()
-    result['sha512'] = sha512(file_content).hexdigest()
+    crc32_value = 0
+    while True:
+        data = the_file.read(BLOCK_SIZE)
+        if data:
+            crc32_value = binascii.crc32(data, crc32_value)
+        else:
+            break
+    crc32_hex_str = hex(crc32_value)[2:]
+    print('{}\t: {}'.format('crc32', crc32_hex_str))
+
+    the_file.seek(0, 0)
+    md5_m = md5()
+    while True:
+        data = the_file.read(BLOCK_SIZE)
+        if data:
+            md5_m.update(data)
+        else:
+            break
+    md5_hex_str = md5_m.hexdigest()
+    print('{}\t: {}'.format('md5', md5_hex_str))
+
+    the_file.seek(0, 0)
+    sha1_m = sha1()
+    while True:
+        data = the_file.read(BLOCK_SIZE)
+        if data:
+            sha1_m.update(data)
+        else:
+            break
+    sha1_hex_str = sha1_m.hexdigest()
+    print('{}\t: {}'.format('sha1', sha1_hex_str))
+
+    the_file.seek(0, 0)
+    sha256_m = sha256()
+    while True:
+        data = the_file.read(BLOCK_SIZE)
+        if data:
+            sha256_m.update(data)
+        else:
+            break
+    sha256_hex_str = sha256_m.hexdigest()
+    print('{}\t: {}'.format('sha256', sha256_hex_str))
+
+    the_file.seek(0, 0)
+    sha512_m = sha512()
+    while True:
+        data = the_file.read(BLOCK_SIZE)
+        if data:
+            sha512_m.update(data)
+        else:
+            break
+    sha512_hex_str = sha512_m.hexdigest()
+    print('{}\t: {}'.format('sha512', sha512_hex_str))
 
     the_file.close()
 
-    print('filename: {}\n'.format(file_path))
-    for k in ['crc32', 'md5', 'sha1', 'sha256', 'sha512']:
-        print('{}\t: {}'.format(k, result[k]))
-    
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
